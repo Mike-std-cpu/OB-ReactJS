ca55a5bb295890c85e846da4f0be9cbc10548215
Mike Andrade <hoodloom07@outlook.es>
2022-11-24 14:59:42 -0600

    5. Age with greetingF and differences about how use it

--- module-1/src/components/pure/greetingF.jsx
+++ module-1/src/components/pure/greetingF.jsx
@@ -1,21 +1,27 @@
 //* Diferencia entre clase y  Componentes Funcion, es que a diferencia de los coponentes clase que tienen sus contructores y propiedad
 //* En cualquier situacion puede devolver un componente html
 
-import React from 'react'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 function Greetingf(props) {
+    //! Breve introduccion a useState
+    //*const ["VARIABLE", "METODO PARA ACTUALIZAR"] = useState("VALOR INICNIAL")
+    const [age, setage] = useState(29)
+    const birtday = () =>{
+        setage(age + 1)
+    }
   return (
     <div>
         <p>
             ¡Hola {props.name} desde componente Funcional!
         </p>
-        {/* <p> Tu edad es {this.state.age}</p> */}
-        {/* <div>
-            <button onClick={this.birtday}>
+         <p> Tu edad es { age }</p>
+        <div>
+            <button onClick={birtday}>
                 Cumplir años
             </button>
-        </div> */}
+        </div>
     </div>
   )
 }
