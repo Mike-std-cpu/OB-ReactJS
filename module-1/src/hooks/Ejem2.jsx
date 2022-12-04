61e26b3a69f1f5121dbafdccc56f09d862980bbe
Mike Andrade <hoodloom07@outlook.es>
2022-12-03 22:32:58 -0600

    14. useEffect() declarate in file

--- module-1/src/hooks/Ejem2.jsx
+++ module-1/src/hooks/Ejem2.jsx
@@ -14,8 +14,26 @@ export const Ejem2 = () => {
     const [contador, setContador] = useState(0) //Que se inicialice en 0
     const [contador2, setContador2] = useState(0)
 
-    //* creacion de una referencia (useRef) para asociar una variable a un evento del DOM de la vista HTML
-    const miRef = useRef()
+    //! creacion de una referencia (useRef) para asociar una variable a un evento del DOM de la vista HTML
+    const miRef = useRef();
+
+    function incrementoUno(){
+        setContador(contador + 1)
+    }
+
+    function incrementarDos(){
+        setContador2(contador2 + 1)
+    }
+
+    //! useEffect para controlar vistas
+    //? Caso 1: Ejecutar siempre un snippet de código. (Cada que haya un cambio estado en el componente se ejecuta el useEffect())
+    useEffect(() => {
+        console.log("Firts: Cambio en el estado del componente")
+        console.log("Mostrando referencia a elemento del DOM:");
+        console.log(miRef)
+    })
+    
+
   
     return (
     <div>Ejem2</div>
